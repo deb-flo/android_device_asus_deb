c990a87a8e8f600949e483d6e6d5545a419f527f
JT1510365 <jontran95@gmail.com>
2019-05-13 13:07:24 +0200

    deb: dot-ify

--- dot_deb.mk
+++ dot_deb.mk
@@ -1,14 +1,11 @@
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1200
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
-
-# Inherit telephony stuff
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+$(call inherit-product, vendor/dot/config/telephony.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
@@ -19,7 +16,7 @@ $(call inherit-product-if-exists, vendor/asus/deb/device-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := deb
-PRODUCT_NAME := lineage_deb
+PRODUCT_NAME := dot_deb
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
