d273588b2490410e69527b89f558a8bbc5cca44a
JT1510365 <jontran95@gmail.com>
2019-06-20 16:45:36 -0400

    deb: Bootleggers-ify

---
+++ bootleg.mk
@@ -0,0 +1,31 @@
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/bootleggers/config/common.mk)
+
+# Inherit telephony stuff
+$(call inherit-product, vendor/lineage/config/telephony.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/deb/device.mk)
+$(call inherit-product-if-exists, vendor/asus/deb/device-vendor.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := deb
+PRODUCT_NAME := bootleg_deb
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7 2013
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razorg \
+    PRIVATE_BUILD_DESC="razorg-user 6.0.1 MOB30X 3036618 release-keys"
+
+BUILD_FINGERPRINT := google/razorg/deb:6.0.1/MOB30X/3036618:user/release-keys
