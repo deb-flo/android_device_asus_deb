613da9d1681f2e7e772df557bd5d6f918ede96f8
ripee <ripee.dev@gmail.com>
2019-07-27 16:32:50 +0200

    deb: Xenon-ify

--- xenonhd_deb.mk
+++ xenonhd_deb.mk
@@ -4,11 +4,11 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1200
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_tablet_wifionly.mk)
+# Inherit some common XenonHD stuff.
+$(call inherit-product, vendor/xenonhd/config/common_mini_tablet_wifionly.mk)
 
 # Inherit telephony stuff
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product, vendor/xenonhd/config/telephony.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
@@ -19,7 +19,7 @@ $(call inherit-product-if-exists, vendor/asus/deb/device-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := deb
-PRODUCT_NAME := lineage_deb
+PRODUCT_NAME := xenonhd_deb
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
